6aaaf99733248a0b608809e0d4cb70aec064352f
mltb <e.anastayyar@gmail.com>
2022-06-10 10:06:27 +0000

    p

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import os
-os.system('ffmpeg -stream_loop -1 -re -i coder.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/cbqs837yss8uv -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 500k -f flv  rtmp://a.rtmp.youtube.com/live2/r8zr-70a2-b85z-2dg4-1yyy')
+os.system('ffmpeg -stream_loop -1 -re -i coder.mp4 -stream_loop -1 -re -i http://pool.anison.fm:9001/AniSonFM(320) -vcodec libx264 -pix_fmt yuvj420p -maxrate 2048k -preset ultrafast -r 12 -framerate 1 -g 50 -crf 51 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 100 -b:v 500k -f flv  rtmp://a.rtmp.youtube.com/live2/dpm6-5uvb-rz5k-d8kx-ddvf')
